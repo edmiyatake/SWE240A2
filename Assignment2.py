52ad8296cb161227dbafb9ce973cc3f67b6dc45f
Edwin Miyatake <edmiyatake@gmail.com>
2024-10-04 15:54:05 -0700

    finished task 3 and started tast 2 and 4

--- Assignment2.py
+++ Assignment2.py
@@ -14,6 +14,38 @@ class Stack:
     def size(self):
         return len(self.data)
 
+def calculator(str):
+    newStack = Stack()
+    
+
+class Queue:
+    def __init__(self):
+        self.front = 0
+        self.data = []
+    
+    def enqueue(self,element):
+        self.data.append(element)
+    
+    def dequeue(self):
+        if self.size() == 0:
+            raise Exception("Queue is empty")
+        else:   
+            solution = self.data[self.front]
+            self.data[self.front] = None
+            self.front += 1
+            return solution
+            
+    
+    def poll(self):
+        return self.data[self.front]
+    
+    def size(self):
+        return len(self.data) - self.front
+
+class stackWithTwoQs:
+    exit
+    
+
 # TASK 1 TEST CASES
 # newStack = Stack()
 # newStack.push(32)
@@ -28,4 +60,20 @@ class Stack:
 # print(newStack.peek())
 # print(newStack.size())
 
-#TASK 2 TEST CASES
+# TASK 2 TEST CASES
+
+# TASK 3 TEST CASES 
+# newQueue = Queue()
+# newQueue.dequeue()
+# newQueue.enqueue(36)
+# print(newQueue.size())
+# newQueue.enqueue(23)
+# print(newQueue.size())
+# newQueue.enqueue(33)
+# print(newQueue.size())
+# newQueue.enqueue(900)
+# print(newQueue.size())
+# print(newQueue.dequeue())
+# print(newQueue.poll())
+
+# TASK 4 TEST CASES
