e885c1968d4a8916d3ef6f02c3c7293f64f5f7eb
Edwin Miyatake <edmiyatake@gmail.com>
2024-10-14 14:35:33 -0700

    removed task 3 and 4 to avoid a billion page submission and easier to scroll to test cases

--- Assignment2.py
+++ Assignment2.py
@@ -6,10 +6,16 @@ class Stack:
         self.data.append(element)
  
     def pop(self):
-        self.data.pop()
+        if not self.data:
+            raise Exception("stack is empty")
+        else:
+            return self.data.pop()
     
     def peek(self):
-        return self.data[-1]
+        if not self.data:
+            raise Exception("stack is empty")
+        else:
+            return self.data[-1]
     
     def size(self):
         return len(self.data)
@@ -28,47 +34,6 @@ def calculator(str):
 
     
 
-class Queue:
-    def __init__(self):
-        self.front = 0
-        self.data = []
-    
-    def enqueue(self,element):
-        self.data.append(element)
-    
-    def dequeue(self):
-        if self.size() == 0:
-            raise Exception("Queue is empty")
-        else:   
-            solution = self.data[self.front]
-            self.data[self.front] = None
-            self.front += 1
-            return solution
-            
-    
-    def poll(self):
-        return self.data[self.front]
-    
-    def size(self):
-        return len(self.data) - self.front
-
-class stackWithTwoQs:
-    def __init__(self):
-        self.queueOne = Queue()
-        self.queueTwo = Queue()
-    
-    def push(self,x):
-        exit
-
-    def pop(self):
-        exit
-    
-    def peek(self):
-        exit
-    
-    def size(self):
-        exit
-
 # TASK 1 TEST CASES
 # newStack = Stack()
 # newStack.push(32)
@@ -84,20 +49,3 @@ class stackWithTwoQs:
 # print(newStack.size())
 
 # TASK 2 TEST CASES
-
-# TASK 3 TEST CASES 
-# newQueue = Queue()
-# newQueue.dequeue()
-# newQueue.enqueue(36)
-# print(newQueue.size())
-# newQueue.enqueue(23)
-# print(newQueue.size())
-# newQueue.enqueue(33)
-# print(newQueue.size())
-# newQueue.enqueue(900)
-# print(newQueue.size())
-# print(newQueue.dequeue())
-# print(newQueue.poll())
-
-# TASK 4 TEST CASES
-print(calculator("50 + 67 * 42"))
